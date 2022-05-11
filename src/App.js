17f9dd49bbfc5fcc263cb65995cb301774bf2378
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-11 10:47:51 +0600

    navbar and banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
-    <div  className="App">
-      <h2 className='text-xl font bold'>Hello world</h2>
-      <button className='btn' ></button>
+    <div  >
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>} ></Route>
+        <Route path='/about' element={<Home></Home>} ></Route>
+        <Route path='/login' element={<Home></Home>} ></Route>
+        <Route path='/' element={<Home></Home>} ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,32 @@
+import React from "react";
+import chair from "../../assets/images/chair.png";
+import bgChair from "../../assets/images/bg.png";
+
+const Banner = () => {
+  return (
+    <div
+      class="hero min-h-screen px-24  "
+      style={{
+        backgroundImage: `url(${bgChair})`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt="" />
+        <div>
+          <h1 class="text-5xl font-bold">Your New Smile Starts Here!</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary text-white font-bold uppercase">
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
